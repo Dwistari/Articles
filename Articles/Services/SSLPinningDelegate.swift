1f3af1edcd8d367cc3d8178b5d292ec28f782123
Dwistari <dwistari16@gmail.com>
2025-04-17 09:29:03 +0700

    fix ssl pinning class

--- Articles/Services/SSLPinningDelegate.swift
+++ Articles/Services/SSLPinningDelegate.swift
@@ -8,17 +8,45 @@
 import Foundation
 
 class SSLPinningDelegate: NSObject, URLSessionDelegate {
-    func urlSession(_ session: URLSession,
-                    didReceive challenge: URLAuthenticationChallenge,
+    
+    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge,
                     completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
         
-        if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust,
-           let serverTrust = challenge.protectionSpace.serverTrust {
-
+        guard let serverTrust = challenge.protectionSpace.serverTrust else {
+            completionHandler(.cancelAuthenticationChallenge, nil)
+            return
+        }
+        
+        // Load local cert
+        guard let certPath = Bundle.main.path(forResource: "spaceflightnewsapi_der", ofType: "cer"),
+              let localCertData = try? Data(contentsOf: URL(fileURLWithPath: certPath)) else {
+            print("❌ Local certificate not found or unreadable")
+            completionHandler(.cancelAuthenticationChallenge, nil)
+            return
+        }
+        
+        // Compare server certs
+        var isPinned = false
+        let certCount = SecTrustGetCertificateCount(serverTrust)
+        for i in 0..<certCount {
+            if let certChain = SecTrustCopyCertificateChain(serverTrust) as? [SecCertificate] {
+                for cert in certChain {
+                    let serverCertData = SecCertificateCopyData(cert) as Data
+                    if serverCertData == localCertData {
+                        isPinned = true
+                        break
+                    }
+                }
+            }
+        }
+        
+        if isPinned {
             let credential = URLCredential(trust: serverTrust)
             completionHandler(.useCredential, credential)
         } else {
+            print("❌ Local certificate not found or unreadable")
             completionHandler(.cancelAuthenticationChallenge, nil)
         }
+        
     }
 }
